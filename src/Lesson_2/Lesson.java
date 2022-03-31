ae036676d74a0fe448e08be7f12105ce3cd41da0
Zymell <sakh_22@mail.ru>
2022-03-31 21:58:40 +0500

    Add third line

--- src/Lesson_2/Lesson.java
+++ src/Lesson_2/Lesson.java
@@ -5,6 +5,7 @@ public class Lesson {
     public static void main(String[] args) {
         System.out.println("Hello qa_guru");
         System.out.println("Add second line");
+        System.out.println("ADD FIRST LINE");
 
 
     }
